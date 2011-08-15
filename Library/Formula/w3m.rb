740067d70bfcaa7cb07b6ae2bdc7156219b415a5
baron <richstyles@gmail.com>
2011-08-15 12:54:04 +0900

    update w3m to the lastest

--- Library/Formula/w3m.rb
+++ Library/Formula/w3m.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class W3m < Formula
-  url 'http://downloads.sourceforge.net/project/w3m/w3m/w3m-0.5.2/w3m-0.5.2.tar.gz'
+  url 'http://downloads.sourceforge.net/project/w3m/w3m/w3m-0.5.3/w3m-0.5.3.tar.gz'
   homepage 'http://w3m.sourceforge.net/'
-  md5 'ba06992d3207666ed1bf2dcf7c72bf58'
+  md5 '1b845a983a50b8dec0169ac48479eacc'
 
   depends_on 'bdw-gc'
 
