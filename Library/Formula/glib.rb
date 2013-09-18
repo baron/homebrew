0b3591e4ec14547d288dc11c57e76b3d574403ac
Sam Baron <richstyles@gmail.com>
2013-09-18 23:10:49 +0900

    glib formula

--- Library/Formula/glib.rb
+++ Library/Formula/glib.rb
@@ -21,7 +21,7 @@ class Glib < Formula
   def patches
     p = {}
     # https://bugzilla.gnome.org/show_bug.cgi?id=673135 Resolved as wontfix.
-    p[:p1] = "https://raw.github.com/gist/5393707/5a9047ab7838709084b36242a44471b02d036386/glib-configurable-paths.patch"
+    p[:p1] = "https://gist.github.com/jacknagel/5393707/raw/5a9047ab7838709084b36242a44471b02d036386/glib-configurable-paths.patch"
     p[:p0] = "https://trac.macports.org/export/95596/trunk/dports/devel/glib2/files/patch-configure.diff" if build.universal?
     p
   end
